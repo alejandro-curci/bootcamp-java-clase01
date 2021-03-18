858701260c9943d3ca4ac69b86efb14e3e016f9c
Alejandro Curci <acurci@ar0c02cw2waml85.ml.com>
2021-03-18 20:48:51 -0300

    Correccion de comentarios

--- integrador/RadixSort.java
+++ integrador/RadixSort.java
@@ -83,48 +83,11 @@ public class RadixSort
         }
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int arr[]={16223,898,13,906,235,23,9,1532,6388,2511,8};
         radixSort(arr);
-        // String strArr[] = StringUtil.toStringArray(arr);
-        // StringUtil.lNormalize(strArr, '0');
-
-        for(int i=0; i<arr.length;i++)
-        {
+        for(int i=0; i<arr.length;i++) {
             System.out.print(arr[i]+(i<arr.length-1?",":""));
         }
-
-        // System.out.println();
-        // for(int i=0; i<strArr.length;i++) {
-            // System.out.print(strArr[i]+(i<strArr.length-1?",":""));
-        // }
     }
 }
-
-/*
-    public static void radixSort(int[] arr) {
-        // convierto todos los elementos en String (y aprovecho a determinar la maxima long)
-        String[] strArr = new String[arr.length];
-        int maxLength = 0;
-        for(int i=0; i<arr.length; i++) {
-            strArr[i] = String.valueOf(arr[i]);
-            if(strArr[i].length()>maxLength) {
-                maxLength = strArr[i].length();
-            }
-        }
-        // agrego ceros a la izquierda
-        for(String s : strArr) {
-            int ceros = maxLength - s.length();
-            while(ceros>0) {
-                s = "0"+s;
-                ceros--;
-            }
-
-        }
-
-
-    }
-}
-
- */
