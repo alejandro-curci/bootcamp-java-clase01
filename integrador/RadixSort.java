8a5ca9ea28b686a0c738e8c89264aa8f6f1fddc6
Alejandro Curci <acurci@ar0c02cw2waml85.ml.com>
2021-03-19 11:32:12 -0300

    Refactor with HashMap

--- integrador/RadixSort.java
+++ integrador/RadixSort.java
@@ -1,7 +1,9 @@
 package integrador;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RadixSort
 {
@@ -17,65 +19,20 @@ public class RadixSort
             ordenada.add(s);
         }
         for(int i=0; i<iteraciones; i++) {
-            List<String> list0 = new ArrayList<>();
-            List<String> list1 = new ArrayList<>();
-            List<String> list2 = new ArrayList<>();
-            List<String> list3 = new ArrayList<>();
-            List<String> list4 = new ArrayList<>();
-            List<String> list5 = new ArrayList<>();
-            List<String> list6 = new ArrayList<>();
-            List<String> list7 = new ArrayList<>();
-            List<String> list8 = new ArrayList<>();
-            List<String> list9 = new ArrayList<>();
+            // el mapa contiene la lista correspondiente a cada digito
+            Map<Integer, List<String>> mapa = new HashMap<>();
+            for(int j=0; j<10; j++) {
+                mapa.put(j, new ArrayList<String>());
+            }
             // ubico cada elemento en la lista que le corresponde
             for(String s : ordenada) {
                 int ubicacion = digitos.indexOf(s.charAt(iteraciones-1-i));
-                switch(ubicacion) {
-                    case 0:
-                        list0.add(s);
-                        break;
-                    case 1:
-                        list1.add(s);
-                        break;
-                    case 2:
-                        list2.add(s);
-                        break;
-                    case 3:
-                        list3.add(s);
-                        break;
-                    case 4:
-                        list4.add(s);
-                        break;
-                    case 5:
-                        list5.add(s);
-                        break;
-                    case 6:
-                        list6.add(s);
-                        break;
-                    case 7:
-                        list7.add(s);
-                        break;
-                    case 8:
-                        list8.add(s);
-                        break;
-                    case 9:
-                        list9.add(s);
-                        break;
-                    default:
-                        break;
-                }
+                mapa.get(ubicacion).add(s);
             }
             ordenada.clear(); // vacio la lista para llenarla con las sublistas
-            ordenada.addAll(list0);
-            ordenada.addAll(list1);
-            ordenada.addAll(list2);
-            ordenada.addAll(list3);
-            ordenada.addAll(list4);
-            ordenada.addAll(list5);
-            ordenada.addAll(list6);
-            ordenada.addAll(list7);
-            ordenada.addAll(list8);
-            ordenada.addAll(list9);
+            for(int j=0; j<10; j++) {
+                ordenada.addAll(mapa.get(j));  // lleno la lista luego de cada paso del algoritmo
+            }
         }
         // copio los elementos ordenados en un Array de enteros
         for(int i=0; i<ordenada.size(); i++) {
@@ -84,10 +41,22 @@ public class RadixSort
     }
 
     public static void main(String[] args) {
-        int arr[]={16223,898,13,906,235,23,9,1532,6388,2511,8};
+        int arr[]={16223,898,13,906,235,23,9,1532,6388,2511,8};  // test 1
+        int arr2[]={90, 1230, 2, 901819, 893, 12, 9021, 5}; // test 2
+        int arr3[]={9023, 876, 98, 34, 6, 1}; // test 3
         radixSort(arr);
+        radixSort(arr2);
+        radixSort(arr3);
         for(int i=0; i<arr.length;i++) {
             System.out.print(arr[i]+(i<arr.length-1?",":""));
+        }
+        System.out.println();
+        for(int i=0; i<arr2.length;i++) {
+            System.out.print(arr2[i]+(i<arr2.length-1?",":""));
+        }
+        System.out.println();
+        for(int i=0; i<arr3.length;i++) {
+            System.out.print(arr3[i]+(i<arr3.length-1?",":""));
         }
     }
 }
